dd4aabba70c952ae83410b85aaa74e72c52a15ac
BenRogozinski <brogodev@gmail.com>
2025-04-05 13:43:32 -0500

    Fixed formatting in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # type: ignore
-from setuptools import setup , find_packages
+from setuptools import setup, find_packages
 from glob import glob
 import os
 
@@ -8,7 +8,6 @@ try:
     from Cython.Distutils import build_ext
 except ImportError:
     from setuptools import Extension
-    
     USING_CYTHON = False
 else:
     USING_CYTHON = True
@@ -18,13 +17,14 @@ sources = glob("logprocessor/libs/*.%s" % (filename_ext,))
 extensions = [
     Extension(source.split(".")[0].replace(os.path.sep, "."),
               sources=[source]
-    )
-for source in sources]
+              )
+    for source in sources
+]
 cmdclass = {"build_ext": build_ext} if USING_CYTHON else {}
 
 setup(
     ext_modules=extensions,
     cmdclass=cmdclass,
-    packages = find_packages() + ["logprocessor.libs"],
+    packages=find_packages() + ["logprocessor.libs"],
     test_suite="tests"
 )
