7868c78632ea7ecd7c43cf4e8393f30f4e735882
BenRogozinski <brogodev@gmail.com>
2025-04-04 22:24:01 -0500

    Added file structure for plugins and main processor.

--- logprocessor/cli.py
+++ logprocessor/cli.py
@@ -1,15 +1,2 @@
-"""
-Logprocessor main script
-
-This script contains all of the logic to use
-Logprocessor as a command-line application
-"""
-
-import argparse
-from . import *
-
-def main():
-    print("Hello world")
-
-if __name__ == "__main__":
-    main()
+def process_log(file_path, **kwargs):
+    ...
